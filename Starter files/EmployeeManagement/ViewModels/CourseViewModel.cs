9c3389e4ce6307fa5dff6234729f31cc9607c973
wiseinvoker <grityangel@gmail.com>
2024-05-19 02:37:18 -0400

    Move files, keep necessary code only

--- Starter files/EmployeeManagement/ViewModels/CourseViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmployeeManagement.ViewModels
-{
-    public class CourseViewModel
-    {
-        public Guid Id { get; set; }
-        public bool IsNew { get; set; }
-        public string Title { get; set; } = string.Empty;
-    }
-}
